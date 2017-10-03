579ea20ee24fb0dbd2f531ebdbc4d71300845c77
Eric Goh Ming Hui <gohminghui88@gmail.com>
2017-10-03 13:00:14 +0800

    Version 1.0

---
+++ JABFR/src/jabfr/JABFR.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jabfr;
+
+/**
+ *
+ * @author Eric M. H. Goh
+ * License: Apache 2.0 Open Source License
+ */
+public class JABFR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Start MainGUI, codes are all in MainGUI class
+        MainGUI mg = new MainGUI();
+        mg.setVisible(true);
+        
+    }
+    
+}
